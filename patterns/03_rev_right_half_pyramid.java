17c6b568cca8892734449da6db855cef63ad0665
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-07-16 20:13:09 +0530

    Create 03_rev_right_half_pyramid.java

---
+++ patterns/03_rev_right_half_pyramid.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main
+{   
+
+	public static void printPattern(int n)
+    {
+        int i, j;
+
+        // outer loop to handle rows
+        for (i = n; i >= 1; i--) {
+
+            // inner loop to handle columns
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+    
+	public static void main(String[] args) {
+		int n = 6;
+        printPattern(n);
+	}
+}
+
+OUTPUT:
+
+******
+*****
+****
+***
+**
+*
