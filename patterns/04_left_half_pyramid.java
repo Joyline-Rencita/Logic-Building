8e4a20f3b8cf244412e9200f312197d32884c638
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-07-16 20:19:10 +0530

    Update 04_left_half_pyramid.java

--- patterns/04_left_half_pyramid.java
+++ patterns/04_left_half_pyramid.java
@@ -3,36 +3,32 @@ import java.util.*;
 public class Main
 {   
 
-	public static void printPattern(int n)
-    {
+	public static void printPattern(int n){
         int i, j;
+        for (i = n; i >= 1; i--) {           // outer loop to handle rows
 
-        // outer loop to handle rows
-        for (i = n; i >= 1; i--) {
-
-            // inner loop to handle columns
-            for (j = 1; j <= i; j++) {
+            for (j = 0; j <= i; j++) {       // inner loop to handle columns
                 System.out.print(" ");
             }
             
-            for (j = 1; j <= n-i; j++) {
+            for (j = 0; j <= n-i; j++) {
                 System.out.print("*");
             }
 
-            // printing new line for each row
             System.out.println();
         }
     }
     
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		int n = 6;
-        printPattern(n);
-	}
+        	printPattern(n);
+    }
 }
 
 OUTPUT : 
-     *
-    **
-   ***
-  ****
- *****
+       *
+      **
+     ***
+    ****
+   *****
+  ******
