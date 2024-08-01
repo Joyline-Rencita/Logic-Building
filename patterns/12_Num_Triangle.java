abda77cc177b9f5d0b94cea7640861493e39ffa2
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-08-01 17:56:24 +0530

    Create 12_Num_Triangle.java

---
+++ patterns/12_Num_Triangle.java
@@ -0,0 +1,28 @@
+public class GeeksForGeeks {
+    public static void printPattern(int n)
+    {
+        int i, j;
+        for (i = 1; i <= n; i++) {                   // outer loop to handle number of rows
+            for (j = 1; j <= n - i; j++) {               // inner loop to print space
+                System.out.print(" ");
+            }
+            for (j = 1; j <= i; j++) {
+                System.out.print(i + " ");                    // inner loop to print star
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
+
+Output : 
+     1 
+    2 2 
+   3 3 3 
+  4 4 4 4 
+ 5 5 5 5 5 
+6 6 6 6 6 6
