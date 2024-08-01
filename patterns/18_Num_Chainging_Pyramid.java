1a22ebbe35472d2a2541f16b2f59d5e06609400e
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-08-01 18:15:39 +0530

    Create 18_Num_Chainging_Pyramid.java

---
+++ patterns/18_Num_Chainging_Pyramid.java
@@ -0,0 +1,27 @@
+public class GeeksForGeeks {
+    public static void printPattern(int n)
+    {
+        int i, j;
+        int num = 1;
+        for (i = 1; i <= n; i++) {                         // outer loop to handle number of rows
+            for (j = 1; j <= i; j++) {                     // inner loop to handle number of columns
+                System.out.print(num + " ");                    // printing value of num in each iteration.
+                num++;                                          // increasing the value of num.
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
+
+Output
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+16 17 18 19 20 21
