e00dc947cb21f8686dd59c604f0b1efdcbdbff39
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-09-06 22:12:58 +0530

    Create 02_arr_mean.java

---
+++ Recursion/02_arr_mean.java
@@ -0,0 +1,22 @@
+import java.io.*; 
+
+class CalcMean 
+{ 
+	// Function Definition of findMean function 
+	static float findMean(int A[], int N) 
+	{ 
+		if (N == 1) 
+			return (float)A[N-1]; 
+		else
+			return ((float)(findMean(A, N-1)*(N-1) + 
+										A[N-1]) / N); 
+	} 
+
+	public static void main (String[] args) 
+	{ 
+		float Mean = 0; 
+		int A[] = {1, 2, 3, 4, 5}; 
+		int N = A.length; 
+		System.out.println(findMean(A, N)); 
+	} 
+} 
