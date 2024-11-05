09942de653ff5090790fe741a9f5f1b7167c15d0
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-11-05 20:22:50 +0530

    Create 04_Post_Order_Traversal.java

---
+++ Queues/04_Post_Order_Traversal.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> stk = new ArrayDeque<>();
+        stk.push(root);
+        while (!stk.isEmpty()) {
+            TreeNode node = stk.pop();
+            ans.addFirst(node.val);
+            if (node.left != null) {
+                stk.push(node.left);
+            }
+            if (node.right != null) {
+                stk.push(node.right);
+            }
+        }
+        return ans;
+    }
+}
