f10d5697b0d6eeea4c292356114628552a161af0
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-11-28 19:58:49 +0530

    Create 08_Letter-combi-of-phone-num.java

---
+++ ArrayList/08_Letter-combi-of-phone-num.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        if (digits.length() == 0) {
+            return ans;
+        }
+        ans.add("");
+        String[] d = new String[] {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        for (char i : digits.toCharArray()) {
+            String s = d[i - '2'];
+            List<String> t = new ArrayList<>();
+            for (String a : ans) {
+                for (String b : s.split("")) {
+                    t.add(a + b);
+                }
+            }
+            ans = t;
+        }
+        return ans;
+    }
+}
+
+
+class Solution {
+    private final String[] d = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    private String digits;
+    private List<String> ans = new ArrayList<>();
+    private StringBuilder t = new StringBuilder();
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) {
+            return ans;
+        }
+        this.digits = digits;
+        dfs(0);
+        return ans;
+    }
+
+    private void dfs(int i) {
+        if (i >= digits.length()) {
+            ans.add(t.toString());
+            return;
+        }
+        String s = d[digits.charAt(i) - '2'];
+        for (char c : s.toCharArray()) {
+            t.append(c);
+            dfs(i + 1);
+            t.deleteCharAt(t.length() - 1);
+        }
+    }
+}
