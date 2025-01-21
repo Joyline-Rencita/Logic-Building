ec00591e3b5f5688c02af93ecc7ad5f62c43b6c1
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-01-21 18:37:03 +0530

    Update 01_Basics.java

--- Graphs/01_Basics.java
+++ Graphs/01_Basics.java
@@ -134,7 +134,7 @@ public class GraphExample {
     }
 }
 
-output:
+Output:
 
 Adjacency List of the graph:
 1: 2 3 
@@ -142,6 +142,7 @@ Adjacency List of the graph:
 3: 1 4 
 4: 2 3 5 
 5: 4 
+    
 DFS traversal starting from vertex 1: 1 2 4 3 5 
 BFS traversal starting from vertex 1: 1 2 3 4 5 
 Deleting edge between 1 and 2
