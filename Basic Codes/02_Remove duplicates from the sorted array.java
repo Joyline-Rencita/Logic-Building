65f2ea1b5cbdb71ca9cc7fdb9361b341992e42d4
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-01-30 15:43:54 +0530

    Create 02_Remove duplicates from the sorted array.java

---
+++ Basic Codes/02_Remove duplicates from the sorted array.java
@@ -0,0 +1,11 @@
+vvclass Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        for (int x : nums) {
+            if (k < 2 || x != nums[k - 2]) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+}
