bbfbf27b57e8700fc9ce80ca0a88ba0b854a6fe8
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-14 18:32:12 +0530

    Create 09_Unique Paths.java

---
+++ Basic Codes/09_Unique Paths.java
@@ -0,0 +1,26 @@
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner 
+(i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+//*************************************
+
+Solution :
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        var f = new int[m][n];
+        f[0][0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i > 0) {
+                    f[i][j] += f[i - 1][j];
+                }
+                if (j > 0) {
+                    f[i][j] += f[i][j - 1];
+                }
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+}
