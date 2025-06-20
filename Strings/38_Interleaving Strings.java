d3206aee9e321ba74dca8d051528d42acb3430fe
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-20 09:53:55 +0530

    Create 38_Interleaving Strings.java

---
+++ Strings/38_Interleaving Strings.java
@@ -0,0 +1,75 @@
+Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+An interleaving of two strings s and t is a configuration where s and t are divided into n and m substrings respectively, such that:
+
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+Note: a + b is the concatenation of strings a and b.
+
+Example 1:
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Explanation: One way to obtain s3 is:
+Split s1 into s1 = "aa" + "bc" + "c", and s2 into s2 = "dbbc" + "a".
+Interleaving the two splits, we get "aa" + "dbbc" + "bc" + "a" + "c" = "aadbbcbcac".
+Since s3 can be obtained by interleaving s1 and s2, we return true.
+
+  Example 2:
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
+Explanation: Notice how it is impossible to interleave s2 with any other string to obtain s3.
+
+  Example 3:
+Input: s1 = "", s2 = "", s3 = ""
+Output: true
+ 
+Constraints:
+
+0 <= s1.length, s2.length <= 100
+0 <= s3.length <= 200
+s1, s2, and s3 consist of lowercase English letters.
+
+
+  Code:
+
+class Solution {
+    private Map<List<Integer>, Boolean> f = new HashMap<>();
+    private String s1;
+    private String s2;
+    private String s3;
+    private int m;
+    private int n;
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        m = s1.length();
+        n = s2.length();
+        if (m + n != s3.length()) {
+            return false;
+        }
+        this.s1 = s1;
+        this.s2 = s2;
+        this.s3 = s3;
+        return dfs(0, 0);
+    }
+
+    private boolean dfs(int i, int j) {
+        if (i >= m && j >= n) {
+            return true;
+        }
+        var key = List.of(i, j);
+        if (f.containsKey(key)) {
+            return f.get(key);
+        }
+        int k = i + j;
+        boolean ans = false;
+        if (i < m && s1.charAt(i) == s3.charAt(k) && dfs(i + 1, j)) {
+            ans = true;
+        }
+        if (!ans && j < n && s2.charAt(j) == s3.charAt(k) && dfs(i, j + 1)) {
+            ans = true;
+        }
+        f.put(key, ans);
+        return ans;
+    }
+}
