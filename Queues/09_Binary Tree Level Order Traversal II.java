ad36bb3dff496561d956747bf06ed8d7c39c15bb
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-07 20:01:12 +0530

    Create 09_Binary Tree Level Order Traversal II.java

---
+++ Queues/09_Binary Tree Level Order Traversal II.java
@@ -0,0 +1,59 @@
+Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[15,7],[9,20],[3]]
+
+Example 2:
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+Input: root = []
+Output: []
+ 
+Constraints:
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> q = new LinkedList<>();
+        q.offerLast(root);
+        while (!q.isEmpty()) {
+            List<Integer> t = new ArrayList<>();
+            for (int i = q.size(); i > 0; --i) {
+                TreeNode node = q.pollFirst();
+                t.add(node.val);
+                if (node.left != null) {
+                    q.offerLast(node.left);
+                }
+                if (node.right != null) {
+                    q.offerLast(node.right);
+                }
+            }
+            ans.addFirst(t);
+        }
+        return ans;
+    }
+}
