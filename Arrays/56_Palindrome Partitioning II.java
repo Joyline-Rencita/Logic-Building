e9f6ee66988632b2b214a5a2047f622679a72259
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-08 19:49:38 +0530

    Create 56_Palindrome Partitioning II.java

---
+++ Arrays/56_Palindrome Partitioning II.java
@@ -0,0 +1,45 @@
+Given a string s, partition s such that every substring of the partition is a palindrome. Return the minimum cuts needed for a palindrome partitioning of s.
+
+Example 1:
+Input: s = "aab"
+Output: 1
+Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cut.
+
+Example 2:
+Input: s = "a"
+Output: 0
+  
+Example 3:
+Input: s = "ab"
+Output: 1
+  
+Constraints:
+1 <= s.length <= 2000
+s consists of lowercase English letters only.
+
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        boolean[][] g = new boolean[n][n];
+        for (var row : g) {
+            Arrays.fill(row, true);
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                g[i][j] = s.charAt(i) == s.charAt(j) && g[i + 1][j - 1];
+            }
+        }
+        int[] f = new int[n];
+        for (int i = 0; i < n; ++i) {
+            f[i] = i;
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (g[j][i]) {
+                    f[i] = Math.min(f[i], j > 0 ? 1 + f[j - 1] : 0);
+                }
+            }
+        }
+        return f[n - 1];
+    }
+}
