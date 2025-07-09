757538c4195d1f4ee2a6e0a99375ad7ade6b8a7e
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-09 22:41:01 +0530

    Update and rename 11_zero_one_triangle.java to 11_Zero_One_Triangle.java

--- patterns/11_Zero_One_Triangle.java
+++ patterns/11_Zero_One_Triangle.java
@@ -4,12 +4,12 @@ public class Main {
     {  
         int i, j;
         for (i = 1; i <= n; i++) {                                     //outer loop to handle number of rows          
-            for (j = 1; j <= i; j++) {                                //inner loop to handle number of columns
-                if ((i + j) % 2 == 0) {                               // if the sum of (i+j) is even then print 1
+            for (j = 1; j <= i; j++) {                                 //inner loop to handle number of columns
+                if ((i + j) % 2 == 0) {                                // if the sum of (i+j) is even then print 1
                     System.out.print(1 + " ");
                 }
                 else {
-                    System.out.print(0 + " ");                        // otherwise print 0
+                    System.out.print(0 + " ");                         // otherwise print 0
                 }
             }
             System.out.println();
