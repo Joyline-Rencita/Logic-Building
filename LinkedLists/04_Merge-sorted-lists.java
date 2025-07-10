17137f27e38dcad5a680808cc4c2d0ff28eabf38
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-10 20:03:23 +0530

    Update and rename 04_merge-sorted-lists.java to 04_Merge-sorted-lists.java

--- LinkedLists/04_Merge-sorted-lists.java
+++ LinkedLists/04_Merge-sorted-lists.java
@@ -1,7 +1,10 @@
 class Solution {
+  
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    
     if (list1 == null || list2 == null)
       return list1 == null ? list2 : list1;
+   
     if (list1.val > list2.val) {
       ListNode temp = list1;
       list1 = list2;
@@ -11,4 +14,5 @@ class Solution {
     return list1;
 
   }
+  
 }
