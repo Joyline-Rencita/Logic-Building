fef885437781de91b41bcc3f9b958885602f08b7
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-17 16:58:59 +0530

    Update 48_Compare Version Numbers.java

--- Strings/48_Compare Version Numbers.java
+++ Strings/48_Compare Version Numbers.java
@@ -26,3 +26,23 @@ Constraints:
 version1 and version2 only contain digits and '.'.
 version1 and version2 are valid version numbers.
 All the given revisions in version1 and version2 can be stored in a 32-bit integer.
+
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int m = version1.length(), n = version2.length();
+        for (int i = 0, j = 0; i < m || j < n; ++i, ++j) {
+            int a = 0, b = 0;
+            while (i < m && version1.charAt(i) != '.') {
+                a = a * 10 + (version1.charAt(i++) - '0');
+            }
+            while (j < n && version2.charAt(j) != '.') {
+                b = b * 10 + (version2.charAt(j++) - '0');
+            }
+            if (a != b) {
+                return a < b ? -1 : 1;
+            }
+        }
+        return 0;
+    }
+}
