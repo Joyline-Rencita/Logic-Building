637b5dfbf155757b8bb7a1718b5349050c61efa7
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-19 22:16:32 +0530

    Create 50_Word Break II.java

---
+++ Strings/50_Word Break II.java
@@ -0,0 +1,86 @@
+Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+
+Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+Output: ["cats and dog","cat sand dog"]
+Example 2:
+
+Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+Explanation: Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: []
+ 
+
+Constraints:
+
+1 <= s.length <= 20
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 10
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+Input is generated in a way that the length of the answer doesn't exceed 105.
+
+
+class Trie {
+    Trie[] children = new Trie[26];
+    boolean isEnd;
+
+    void insert(String word) {
+        Trie node = this;
+        for (char c : word.toCharArray()) {
+            c -= 'a';
+            if (node.children[c] == null) {
+                node.children[c] = new Trie();
+            }
+            node = node.children[c];
+        }
+        node.isEnd = true;
+    }
+
+    boolean search(String word) {
+        Trie node = this;
+        for (char c : word.toCharArray()) {
+            c -= 'a';
+            if (node.children[c] == null) {
+                return false;
+            }
+            node = node.children[c];
+        }
+        return node.isEnd;
+    }
+}
+
+class Solution {
+    private Trie trie = new Trie();
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        for (String w : wordDict) {
+            trie.insert(w);
+        }
+        List<List<String>> res = dfs(s);
+        return res.stream().map(e -> String.join(" ", e)).collect(Collectors.toList());
+    }
+
+    private List<List<String>> dfs(String s) {
+        List<List<String>> res = new ArrayList<>();
+        if ("".equals(s)) {
+            res.add(new ArrayList<>());
+            return res;
+        }
+        for (int i = 1; i <= s.length(); ++i) {
+            if (trie.search(s.substring(0, i))) {
+                for (List<String> v : dfs(s.substring(i))) {
+                    v.add(0, s.substring(0, i));
+                    res.add(v);
+                }
+            }
+        }
+        return res;
+    }
+}
