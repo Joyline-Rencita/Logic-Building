fa77f80af6e1738bac8dbbde8d32ad1ef9b0f3bc
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-29 20:16:39 +0530

    Create 10_Evaluate Reverse Polish Notation.java

---
+++ Deque/10_Evaluate Reverse Polish Notation.java
@@ -0,0 +1,70 @@
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+ 
+
+Constraints:
+
+1 <= tokens.length <= 104
+tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stk = new ArrayDeque<>();
+        for (String t : tokens) {
+            if (t.length() > 1 || Character.isDigit(t.charAt(0))) {
+                stk.push(Integer.parseInt(t));
+            } else {
+                int y = stk.pop();
+                int x = stk.pop();
+                switch (t) {
+                case "+":
+                    stk.push(x + y);
+                    break;
+                case "-":
+                    stk.push(x - y);
+                    break;
+                case "*":
+                    stk.push(x * y);
+                    break;
+                default:
+                    stk.push(x / y);
+                    break;
+                }
+            }
+        }
+        return stk.pop();
+    }
+}
