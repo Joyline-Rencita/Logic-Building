e2276b7653930919da64262cff8119e7e135054d
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-12-16 18:06:46 +0530

    Update inventoy_process_mining.sql

--- ADDITIONALS/inventoy_process_mining.sql
+++ ADDITIONALS/inventoy_process_mining.sql
@@ -23,3 +23,50 @@ SUM(
 )
 
 
+Excess Inventory :
+CASE
+  WHEN
+    SUM(
+      PU_SUM(
+        "o_celonis_MaterialMasterPlant",
+        TO_INT("o_custom_StorageLocation"."UnrestrictedStock")
+      )
+    )
+    >
+    SUM(
+      PU_SUM(
+        "o_celonis_MaterialMasterPlant",
+        CASE
+          WHEN
+            "o_celonis_SalesOrderScheduleLine"."ConfirmedDeliveryDate"
+            <= ADD_MONTHS(TODAY(), 3)
+          THEN
+            "o_celonis_SalesOrderScheduleLine"."ConfirmedQuantity"
+          ELSE 0
+        END
+      )
+    )
+  THEN
+    SUM(
+      PU_SUM(
+        "o_celonis_MaterialMasterPlant",
+        TO_INT("o_custom_StorageLocation"."UnrestrictedStock")
+      )
+    )
+    -
+    SUM(
+      PU_SUM(
+        "o_celonis_MaterialMasterPlant",
+        CASE
+          WHEN
+            "o_celonis_SalesOrderScheduleLine"."ConfirmedDeliveryDate"
+            <= ADD_MONTHS(TODAY(), 3)
+          THEN
+            "o_celonis_SalesOrderScheduleLine"."ConfirmedQuantity"
+          ELSE 0
+        END
+      )
+    )
+  ELSE 0
+END
+
